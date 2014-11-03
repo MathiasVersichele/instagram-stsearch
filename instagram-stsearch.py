42e3f373e40ec2ecd29fa929a4d78eb00891bfb6
mathiasversichele <mathias.versichele@gmail.com>
2014-11-03 09:18:22 +0100

    Check for unique photos, smaller radius, separator
    Changed radius from 5000m to 4900m because downloaded data always had
    some vertical bands with missing data. Seems ok now.

--- instagram-stsearch.py
+++ instagram-stsearch.py
@@ -7,6 +7,7 @@ from dateutil import parser
 import numpy as np
 import urllib2
 import json
+from sets import Set
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -22,12 +23,12 @@ argparser.add_argument("lat_max", help="bounding box maximum latitude", type=flo
 argparser.add_argument("t_min", help="minimum timestamp", type=str)
 argparser.add_argument("t_max", help="maximum timestamp", type=str)
 argparser.add_argument("output", help="output location", type=str)
-argparser.add_argument("-r", help="search radius (default 5000m)", type=int, default=5000)
+argparser.add_argument("-r", help="search radius (default 5000m)", type=int, default=4900)
 argparser.add_argument("-k", help="show search circles in specified kml file", action="store_true")
 args = argparser.parse_args()	
 
 ## static vars
-ig_max_time_span = 5
+ig_max_time_span = 7
 circle_packing = 1.3
 ##
 
@@ -57,6 +58,9 @@ t_max_list = [t_max - datetime.timedelta(days=x*ig_max_time_span) for x in range
 t_min_list = [t_max - datetime.timedelta(days=x*ig_max_time_span) for x in range(1, (days/ig_max_time_span) + 2)]
 t_min_list = [t_min if x<t_min else x for x in t_min_list]
 
+#print t_max_list
+#print t_min_list
+
 lon_list = np.arange(args.lon_min, args.lon_max, change_in_longitude((args.lat_min + args.lat_max)/2, (args.r/1000)*circle_packing))
 lat_list = np.arange(args.lat_min, args.lat_max, change_in_latitude((args.r/1000)*circle_packing))
 
@@ -74,9 +78,10 @@ if args.k:
 total_calls = len(lon_list)*len(lat_list)*len(t_max_list)
 print total_calls, "calls in total without counting recursive calls"
 
+downloaded_photo_ids = Set([])
 call = 1
 f = open(args.output, "a")
-f.write('created;type;link;user_id;user_name;photo_id;lon;lat;tags\n')
+f.write('created|type|link|user_id|user_name|photo_id|lon|lat|tags\n')
 for i in range(0, len(t_max_list)):
 	t1 = t_min_list[i]
 	t2 = t_max_list[i]
@@ -87,18 +92,18 @@ for i in range(0, len(t_max_list)):
 	for j in range(0,len(lon_list)):
 		lon = lon_list[j]
 		for k in range(0,len(lat_list)):
+			print 'call', call, '(', round((float(call)/float(total_calls)) * 100, 3), '%)'
 			lat = lat_list[k]
-			#print lon, lat
 			t2_rec = t2_unix
 			while True:
-				print t1, t2, lon, lat
+				print '  ', t1, t2, lon, lat, time.ctime(float(t2_rec))
 				try:
-					print 'call', call, '(', round((float(call)/float(total_calls)) * 100, 3), '%)'
 					url = 'https://api.instagram.com/v1/media/search?lat=' + str(lat) + '&lng=' + str(lon) + '&access_token=' + args.ig_access_token + '&distance=' + str(args.r) + '&min_timestamp=' + str(t1_unix) + '&max_timestamp=' + str(t2_rec)
-					print url
+					print '  ', url
 					response = urllib2.urlopen(url, None, 5)
 					data = json.load(response)
-					print len(data['data'])
+					print '  ', len(data['data'])
+					new_photos = 0
 					for photo in range(0, len(data['data'])):
 						created = datetime.datetime.fromtimestamp(float(data['data'][photo]['created_time'])).strftime("%Y-%m-%d %H:%M:%S")
 						type = data['data'][photo]['type']
@@ -110,15 +115,27 @@ for i in range(0, len(t_max_list)):
 						user_name = data['data'][photo]['user']['username']
 						tags = data['data'][photo]['tags']
 						tags = [x.encode('utf-8') for x in tags]
-						f.write(created + ';' + type + ';' + link + ';' + user_id + ';' + user_name + ';' + photo_id + ';' + str(location_lon) + ';' + str(location_lat) + ';' + ','.join(tags) + '\n')
-					call = call + 1
-					if(len(data['data']) == 20):
-						t2_rec = data['data'][19]['created_time']
-					else:
+						if not photo_id in downloaded_photo_ids:
+							print '    ', created, '  ', photo_id
+							f.write(created + '|' + type + '|' + link + '|' + user_id + '|' + user_name + '|' + photo_id + '|' + str(location_lon) + '|' + str(location_lat) + '|' + ','.join(tags) + '\n')
+							downloaded_photo_ids.add(photo_id)
+							new_photos = new_photos + 1
+						else:
+							print '    ', created, '  ', photo_id, '*'
+					#if(len(data['data']) == 20):
+					if new_photos == 0:
 						break
+					#elif len(data['data']) == 1 and last_photo_id == photo_id:
+					#	break
+					#elif len(data['data']) > 0:
+					else:
+						#t2_rec = data['data'][19]['created_time']
+						t2_rec = int(data['data'][len(data['data'])-1]['created_time']) - 1
+					
 				except Exception as e:
 					print e
 					print 'waiting 1 minute...'
 					time.sleep(60)
+			call = call + 1
 f.close()
 print "done !"
